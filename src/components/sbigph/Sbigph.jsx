8afddfe1a85c9302dee5807551815ee05b2cdbb2
Yorissh79 <yusifbsh-azmp203@code.edu.az>
2025-04-12 22:20:19 +0400

    added

--- src/components/sbigph/Sbigph.jsx
+++ src/components/sbigph/Sbigph.jsx
@@ -3,8 +3,24 @@ import style from './Sbigph.module.scss'
 
 const Sbigph = () => {
   return (
-    <div className={style.main}>
-      
+    <div className={style.main} id='special'>
+
+        <div className={style.container}>
+
+          <p className={style.f}>Special Promo</p>
+          <p className={style.s}>Summer Sale</p>
+          <p className={style.t}>Repudiandae nostrum natus excepturi fuga ullam accusantium vel ut eveniet aut consequatur laboriosam ipsam.</p>
+
+          <div className={style.cards}>
+
+            
+
+          </div>
+
+          <button className={style.btn}>SHOP NOW</button>
+          
+        </div>
+
     </div>
   )
 }
